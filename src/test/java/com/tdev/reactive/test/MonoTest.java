37e282ea72fda0c5d7f288fb3fc86939f69cace4
Thiago Henrique <thiagonascimento95@hotmail.com>
2020-08-20 20:38:26 -0300

    add dependencies

---
+++ src/test/java/com/tdev/reactive/test/MonoTest.java
@@ -0,0 +1,13 @@
+package com.tdev.reactive.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class MonoTest {
+
+    @Test
+    public void test() {
+        log.info("Everything working as intended");
+    }
+}
