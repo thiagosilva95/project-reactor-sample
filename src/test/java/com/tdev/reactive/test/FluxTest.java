d3d88862ab0e989467d42430f9dea4d8ff2473cb
Thiago Henrique <thiagonascimento95@hotmail.com>
2020-08-21 14:28:31 -0300

    flux auto connect

--- src/test/java/com/tdev/reactive/test/FluxTest.java
+++ src/test/java/com/tdev/reactive/test/FluxTest.java
@@ -217,4 +217,20 @@ public class FluxTest {
                 .verify();
     }
 
+    @Test
+    public void connectableFluxAutoConnect() throws InterruptedException {
+        Flux<Integer> fluxAutoConnect = Flux.range(1, 5)
+                .log()
+                .delayElements(Duration.ofMillis(100))
+                .publish()
+                .autoConnect(2);
+        
+        StepVerifier
+                .create(fluxAutoConnect)
+                .then(fluxAutoConnect::subscribe)
+                .expectNext(1, 2, 3, 4, 5)
+                .expectComplete()
+                .verify();
+    }
+
 }
