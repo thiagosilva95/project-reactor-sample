ff54cebafcda708847e8630d8cdd2f866459f6cd
Thiago Henrique <thiagonascimento95@hotmail.com>
2020-08-25 20:04:39 -0300

    add blockhound dependencies

--- src/test/java/com/tdev/reactive/test/MonoTest.java
+++ src/test/java/com/tdev/reactive/test/MonoTest.java
@@ -1,10 +1,18 @@
 package com.tdev.reactive.test;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.reactivestreams.Subscription;
+import reactor.blockhound.BlockHound;
+import reactor.blockhound.BlockingOperationError;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
+
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 /**
@@ -23,6 +31,27 @@ import reactor.test.StepVerifier;
  * 3. There is an error. (onError) -> Subscriber and subscription will be canceled
  */
 public class MonoTest {
+
+    @BeforeAll
+    public static void setup() {
+        BlockHound.install();
+    }
+
+    @Test
+    public void blockHoundWorks() {
+        try {
+            FutureTask<?> task = new FutureTask<>(() -> {
+                Thread.sleep(0);
+                return "";
+            });
+            Schedulers.parallel().schedule(task);
+
+            task.get(10, TimeUnit.SECONDS);
+            Assertions.fail("should fail");
+        } catch (Exception e) {
+            Assertions.assertTrue(e.getCause() instanceof BlockingOperationError);
+        }
+    }
 
     @Test
     public void monoSubscriber() {
