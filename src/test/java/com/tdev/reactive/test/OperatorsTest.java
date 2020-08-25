35ad6249d3548386ffb53702cda2523c8af761ff
Thiago Henrique <thiagonascimento95@hotmail.com>
2020-08-24 21:56:35 -0300

    concat and combine latest

--- src/test/java/com/tdev/reactive/test/OperatorsTest.java
+++ src/test/java/com/tdev/reactive/test/OperatorsTest.java
@@ -196,4 +196,51 @@ public class OperatorsTest {
     private  Flux<Object> emptyFlux() {
         return Flux.empty();
     }
+
+    @Test
+    public void concatOperator() {
+        Flux<String> flux1 = Flux.just("a", "b");
+        Flux<String> flux2 = Flux.just("c", "d");
+
+        Flux<String> concatFlux = Flux.concat(flux1, flux2).log();
+
+        StepVerifier
+                .create(concatFlux)
+                .expectSubscription()
+                .expectNext("a", "b", "c", "d")
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    public void concatWithOperator() {
+        Flux<String> flux1 = Flux.just("a", "b");
+        Flux<String> flux2 = Flux.just("c", "d");
+
+        Flux<String> concatFlux = flux1.concatWith(flux2).log();
+
+        StepVerifier
+                .create(concatFlux)
+                .expectSubscription()
+                .expectNext("a", "b", "c", "d")
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    public void combineLatestOperator() {
+        Flux<String> flux1 = Flux.just("a", "b");
+        Flux<String> flux2 = Flux.just("c", "d");
+
+        Flux<String> combineLatest = Flux.combineLatest(flux1, flux2,
+                (s1, s2) -> s1.toUpperCase() + s2.toUpperCase())
+                .log();
+
+        StepVerifier
+                .create(combineLatest)
+                .expectSubscription()
+                .expectNext("BC", "BD")
+                .expectComplete()
+                .verify();
+    }
 }
